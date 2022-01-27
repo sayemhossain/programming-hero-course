ce0cd3d9f08b694318f94dc345144418a1011bb3
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-01-27 12:32:51 +0600

    string concat

--- milestone-4/module-18/array.js
+++ milestone-4/module-18/array.js
@@ -1,7 +1,6 @@
-var age = [12, 14, 16, 43, 87];
+var age = [12, 14, 16, 43, 87, 23, 54, 65];
 console.log(age.length);
 
 var totalIndex = age.indexOf;
 console.log(totalIndex);
 console.log(age.indexOf(16));
-d;
---
+++ milestone-4/module-18/pushPop.js
@@ -0,0 +1,11 @@
+var last = ["kalam", "balam", "salam"];
+last.push("malam");
+last.push("talam");
+console.log(last);
+last.pop();
+console.log(last);
+
+var firstName = "Sayem";
+var lastName = "Hossain";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
