e933e2fb36a1060f5a61679cc72b13f9d850e4b1
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-01-29 04:14:37 +0600

    simple problem solving

---
+++ milestone-4/module-18/problem-2.js
@@ -0,0 +1,10 @@
+var signal = "red";
+if (signal == "red") {
+  console.log("Danger! Don't move.");
+} else if (signal == "yellow") {
+  console.log("It's yellow! Just wait a few secound.");
+} else if (signal == "green") {
+  console.log("Yes! You can go now");
+} else {
+  console.log("Something wrong");
+}
