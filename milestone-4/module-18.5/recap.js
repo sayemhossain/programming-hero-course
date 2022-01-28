43b7d5b00e965686870da729e5023fae0b205655
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-01-28 21:36:01 +0600

    new code added

---
+++ milestone-4/module-18.5/recap.js
@@ -0,0 +1,11 @@
+var favoriteBook = "math";
+var bookList = ["bangla", "english", "physic", "higher math"];
+
+var physicBookIndex = bookList.indexOf("physic");
+console.log(physicBookIndex);
+bookList[1] = "biology";
+console.log(bookList);
+bookList.push("english");
+console.log(bookList);
+bookList.pop("english");
+console.log(bookList);
