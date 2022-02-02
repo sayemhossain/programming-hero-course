ee958e222e8d518d87b61a835e9791d5d04e0a59
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-03 02:03:14 +0600

    new code added

--- milestone-4/module-21/fibonacci.js
+++ milestone-4/module-21/fibonacci.js
@@ -1,17 +1,23 @@
 /*
 nth=(n-1)th+(n-2)th
 */
-const fibo = [0, 1];
-for (let i = 2; i <= 12; i++) {
-  fibo[i] = fibo[i - 1] + fibo[i - 2];
-}
+// const fibo = [0, 1];
+// for (let i = 2; i <= 12; i++) {
+//   fibo[i] = fibo[i - 1] + fibo[i - 2];
+// }
 // console.log(fibo);
 
 function fabonacciSeries(num) {
-  var fibo = [0, 1];
-  for (let i = 2; i <= num; i++) {
-    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  if (typeof num != "number") {
+    return "Please give a valid number!";
+  } else if (num < 2) {
+    return "Please enter a positive number greater then 1";
+  } else {
+    var fibo = [0, 1];
+    for (let i = 2; i <= num; i++) {
+      fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
   }
-  return fibo;
 }
-console.log(fabonacciSeries(12));
+console.log(fabonacciSeries(23));
