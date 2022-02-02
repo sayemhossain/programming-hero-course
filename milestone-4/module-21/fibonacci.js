e7b0c44eb36700fcf1526600b11523f8a2123ea6
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-03 01:57:28 +0600

    new code added

--- milestone-4/module-21/fibonacci.js
+++ milestone-4/module-21/fibonacci.js
@@ -5,4 +5,13 @@ const fibo = [0, 1];
 for (let i = 2; i <= 12; i++) {
   fibo[i] = fibo[i - 1] + fibo[i - 2];
 }
-console.log(fibo);
+// console.log(fibo);
+
+function fabonacciSeries(num) {
+  var fibo = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+  return fibo;
+}
+console.log(fabonacciSeries(12));
