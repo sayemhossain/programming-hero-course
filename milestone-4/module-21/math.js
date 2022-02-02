78071c6877501e8138a44f0ac44f0fc33cf7e2eb
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-02 23:52:17 +0600

    new code added

--- milestone-4/module-21/math.js
+++ milestone-4/module-21/math.js
@@ -3,5 +3,7 @@
 const myNumber = 22.334;
 // const output = Math.ceil(myNumber);
 // const output = Math.floor(myNumber);
-const output = Math.round(myNumber);
-console.log(output);
+// const output = Math.round(myNumber);
+const output = Math.random() * 10;
+const floor = Math.floor(output);
+console.log(floor);
---
+++ milestone-4/module-21/swap.js
@@ -0,0 +1,3 @@
+var first = 4;
+var second = 5;
+console.log(first, second);
