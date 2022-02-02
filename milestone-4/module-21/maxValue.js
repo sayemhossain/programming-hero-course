b9949e9010e647055f26b278ea0b4902ae7c5a93
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-03 00:12:06 +0600

    new code added

--- milestone-4/module-21/maxValue.js
+++ milestone-4/module-21/maxValue.js
@@ -1,5 +1,5 @@
 var a = 134;
-var b = 54;
+var b = 534;
 var c = 93;
 // if (a > b) {
 //   console.log("a is big");
@@ -7,10 +7,27 @@ var c = 93;
 //   console.log("b is big");
 // }
 
-if (a > b && a > c) {
-  console.log("a is bigger");
-} else if (b > a && b > c) {
-  console.log("b is bigger");
-} else {
-  console.log("c is bigger");
+// if (a > b && a > c) {
+//   console.log("a is bigger");
+// } else if (b > a && b > c) {
+//   console.log("b is bigger");
+// } else {
+//   console.log("c is bigger");
+// }
+
+// var max = Math.max(a, b, c);
+// console.log(max);
+
+function findMax(a, b, c) {
+  //   if (a > b) {
+  //     console.log("a is bigger");
+  //   } else {
+  //     console.log("b is bigger");
+  //   }
+  var max = Math.max(a, b, c);
+  console.log("max " + max);
+
+  var min = Math.min(a, b, c);
+  console.log("min " + min);
 }
+findMax(a, b, c);
