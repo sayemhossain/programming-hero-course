064c3397a0713597638fefcd68a0dfd67507d9cf
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-02 20:57:57 +0600

    new code added

--- milestone-4/module-21/math.js
+++ milestone-4/module-21/math.js
@@ -1,3 +1,7 @@
-const myNumber = -5;
-const output = Math.abs(myNumber);
+// const myNumber = -5;
+// const output = Math.abs(myNumber);
+const myNumber = 22.334;
+// const output = Math.ceil(myNumber);
+// const output = Math.floor(myNumber);
+const output = Math.round(myNumber);
 console.log(output);
