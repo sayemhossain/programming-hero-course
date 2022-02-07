138cc976de5fd11536e477c0d26743de6c521de8
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-08 00:47:14 +0600

    new code added

--- milestone-5/module-25/moreEvent.js
+++ milestone-5/module-25/moreEvent.js
@@ -1,3 +1,26 @@
 document.getElementById("btn").addEventListener("click", function () {
   document.getElementById("info").style.display = "none";
+  document.getElementById("delete-confirm").value = "";
 });
+// focus
+// document
+//   .getElementById("delete-confirm")
+//   .addEventListener("focus", function () {
+//     document.body.style.backgroundColor = "lightcoral";
+//   });
+
+// // blur
+// document.getElementById("delete-confirm").addEventListener("blur", function () {
+//   document.body.style.backgroundColor = "white";
+// });
+
+document
+  .getElementById("delete-confirm")
+  .addEventListener("keyup", function (event) {
+    const btn = document.getElementById("btn");
+    if (event.target.value == "delete") {
+      btn.removeAttribute("disabled");
+    } else {
+      btn.setAttribute("disabled", true);
+    }
+  });
