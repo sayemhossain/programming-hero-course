01e8660c42cbd74a3837c1006e62c9bec87cae12
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-10 01:26:50 +0600

    new code added

---
+++ milestone-5/module.26/baperBank.js
@@ -0,0 +1,12 @@
+document.getElementById("login-btn").addEventListener("click", function () {
+  const email = document.getElementById("user-email");
+  const userEmail = email.value;
+  const pass = document.getElementById("user-pass");
+  const userPass = pass.value;
+
+  if (userEmail == "mim@gmail.com" && userPass == "sayem") {
+    window.location.href = "banking.html";
+  } else {
+    console.log("Invalid User");
+  }
+});
