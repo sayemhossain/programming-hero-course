f2193abd7fb5ed01108d7bc57602eeb6c35ed118
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-10 19:01:42 +0600

    withdraw done

--- milestone-5/module.26/bank.js
+++ milestone-5/module.26/bank.js
@@ -1,14 +1,49 @@
+// this is for deposit
 document.getElementById("deposit-btn").addEventListener("click", function () {
   // get the amount deposit
   const depositInput = document.getElementById("deposit-amount");
-  const newDepositAmount = depositInput.value;
+  const newDepositText = depositInput.value;
+  const newDepositAmount = parseFloat(newDepositText);
   //   showing in deposit
   const totalDeposit = document.getElementById("totalDeposit");
-  const previousDepositAmount = totalDeposit.innerText;
+  const previousDepositText = totalDeposit.innerText;
+  const previousDepositAmount = parseFloat(previousDepositText);
   // add money
-  const totalNewDeposit =
-    parseFloat(previousDepositAmount) + parseFloat(newDepositAmount);
+  const totalNewDeposit = previousDepositAmount + newDepositAmount;
   totalDeposit.innerText = totalNewDeposit;
+
+  // update account balance
+  const totalBalance = document.getElementById("totalBalance");
+  const previousTotalBalanceText = totalBalance.innerText;
+  const previousTotalBalance = parseFloat(previousTotalBalanceText);
+  const totalNewBalance = previousTotalBalance + newDepositAmount;
+  totalBalance.innerText = totalNewBalance;
+
   //   clear deposit input feild
   depositInput.value = "";
+});
+
+// this is for withdraw
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  // get the amount withdraw
+  const withdrawInput = document.getElementById("withdraw-amount");
+  const newWithdrawText = withdrawInput.value;
+  const newWithdrawAmount = parseFloat(newWithdrawText);
+  //   showing in withdraw
+  const totalWithdraw = document.getElementById("totalWithdraw");
+  const previousWithdrawText = totalWithdraw.innerText;
+  const previousWithdrawAmount = parseFloat(previousWithdrawText);
+  // add money
+  const totalNewWithdraw = previousWithdrawAmount + newWithdrawAmount;
+  totalWithdraw.innerText = totalNewWithdraw;
+
+  // update account balance
+  const totalBalance = document.getElementById("totalBalance");
+  const previousTotalBalanceText = totalBalance.innerText;
+  const previousTotalBalance = parseFloat(previousTotalBalanceText);
+  const totalNewBalance = previousTotalBalance - newWithdrawAmount;
+  totalBalance.innerText = totalNewBalance;
+
+  //   clear whithdraw input feild
+  withdrawInput.value = "";
 });
