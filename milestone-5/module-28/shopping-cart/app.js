357c51ba360a2e59d98ffdf510f0fc8ae4b6d5f9
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-13 02:57:15 +0600

    new code added

--- milestone-5/module-28/shopping-cart/app.js
+++ milestone-5/module-28/shopping-cart/app.js
@@ -1,1 +1,24 @@
-console.log('I am in a separate file')
+function productUpdateNumber(product, isIncreasing) {
+  const productinput = document.getElementById(product + "-number");
+  const productNumber = parseInt(productinput.value);
+
+  if (isIncreasing == true) {
+    productinput.value = parseInt(productinput.value) + 1;
+  } else if (productNumber > 0) {
+    productinput.value = parseInt(productinput.value) - 1;
+  }
+}
+// this is for phone
+document.getElementById("phone-plus").addEventListener("click", function () {
+  productUpdateNumber("phone", true);
+});
+document.getElementById("phone-minus").addEventListener("click", function () {
+  productUpdateNumber("phone", false);
+});
+// this is for case
+document.getElementById("phone-plus").addEventListener("click", function () {
+  productUpdateNumber("phone", true);
+});
+document.getElementById("phone-minus").addEventListener("click", function () {
+  productUpdateNumber("phone", false);
+});
