d22f301e43493432e595ae1fd0bd9ae163566a9c
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-13 18:08:47 +0600

    new code added

--- milestone-5/module-28/pin-matcher/app.js
+++ milestone-5/module-28/pin-matcher/app.js
@@ -2,3 +2,7 @@ document.getElementById("pin-btn").addEventListener("click", function () {
   const pin = parseInt(Math.random() * 9000 + 900);
   document.getElementById("pin-feild").value = pin;
 });
+document.getElementById("number").addEventListener("click", function (event) {
+  const typedNumber = event.target.innerText;
+  document.getElementById("number-feild").value = typedNumber;
+});
