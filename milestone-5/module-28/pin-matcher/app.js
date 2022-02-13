35c885fdc75959ae2916cf00a84ecdde5a301e3e
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-13 19:31:46 +0600

    new code added

--- milestone-5/module-28/pin-matcher/app.js
+++ milestone-5/module-28/pin-matcher/app.js
@@ -2,9 +2,47 @@ document.getElementById("pin-btn").addEventListener("click", function () {
   const pin = parseInt(Math.random() * 9000 + 900);
   document.getElementById("pin-feild").value = pin;
 });
+
+// calculator
 document.getElementById("number").addEventListener("click", function (event) {
-  const newNumber = event.target.innerText;
-  let typedNumber = document.getElementById("number-feild");
-  const previousNumber = typedNumber.value;
-  typedNumber.value = previousNumber + newNumber;
+  const number = event.target.innerText;
+  let calcNumber = document.getElementById("number-feild");
+
+  if (isNaN(number)) {
+    if (number == "C") {
+      calcNumber.value = "";
+    }
+  } else {
+    const previousNumber = calcNumber.value;
+    const newNumber = previousNumber + number;
+    calcNumber.value = newNumber;
+  }
+});
+// verifying
+document.getElementById("submit-btn").addEventListener("click", function () {
+  const pinValue = document.getElementById("pin-feild");
+  let pin = pinValue.value;
+  const calcValue = document.getElementById("number-feild");
+  let number = calcValue.value;
+  const match = document.getElementById("match");
+  const notMatch = document.getElementById("not-match");
+  if (pin == number) {
+    match.style.display = "block";
+    notMatch.style.display = "none";
+  } else {
+    notMatch.style.display = "block";
+    match.style.display = "none";
+    const tryLeft = document.getElementById("try-left");
+    let tryNumber = parseInt(tryLeft.innerText) - 1;
+
+    if (tryNumber >= 0) {
+      if (tryNumber == 0) {
+        document.getElementById("faild-message").style.display = "block";
+        document.getElementById("try").style.display = "none";
+      }
+      tryLeft.innerText = tryNumber;
+    }
+  }
+  pinValue.value = "";
+  calcValue.value = "";
 });
