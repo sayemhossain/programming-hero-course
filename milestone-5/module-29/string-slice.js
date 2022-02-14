e9790556e32877612ac3187613306c033e2754a0
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-14 14:57:50 +0600

    new code added

---
+++ milestone-5/module-29/string-slice.js
@@ -0,0 +1,12 @@
+const anthem = "Amar sonar Bangla ami tomai valobashi";
+
+//splice
+const words = anthem.split(" ");
+const withOutA = anthem.split("a");
+// console.log(withOutA);
+
+//slice
+const smallSlice = anthem.slice(5, 13);
+// console.log(smallSlice);
+
+//substr
