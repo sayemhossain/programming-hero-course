60dd781e615bde9a7dfea0ea4149f384e54a5ed8
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-14 15:07:24 +0600

    new code added

--- milestone-5/module-29/string-slice.js
+++ milestone-5/module-29/string-slice.js
@@ -10,3 +10,20 @@ const smallSlice = anthem.slice(5, 13);
 // console.log(smallSlice);
 
 //substr
+const anoterPart = anthem.substr(11, 8);
+// console.log(anoterPart);
+
+//subString
+const anotherAnotherPart = anthem.substring(11, 15);
+// console.log(anotherAnotherPart);
+
+//concat
+const first = "amader ";
+const second = "city";
+// const fullString = first + second;
+// console.log(first.concat(second).concat(" Dhaka"));
+
+//join
+const word = ["a", "b", "c", "d"];
+const allJoined = word.join(", ");
+console.log(allJoined);
