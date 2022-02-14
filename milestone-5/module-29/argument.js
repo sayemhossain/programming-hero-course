2d4a00a053b57cef144c704cf9b9f35b465955d1
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-15 03:45:42 +0600

    new code

--- milestone-5/module-29/argument.js
+++ milestone-5/module-29/argument.js
@@ -1,7 +1,11 @@
 function addNumber(num1, num2) {
-  console.log(arguments);
-  const result = num1 + num2;
+  // console.log(arguments[1]);
+  let result = 0;
+  for (const num of arguments) {
+    result = result + num;
+  }
+  // const result = num1 + num2;
   return result;
 }
-const sum = addNumber(23, 13, 50, 100);
+const sum = addNumber(23, 13, 50, 100, 420);
 console.log(sum);
