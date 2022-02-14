3df20ee74decef6c9b9b58f8c0b55c408962bad6
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-14 21:34:55 +0600

    new code added

--- milestone-5/module-29/array-sort.js
+++ milestone-5/module-29/array-sort.js
@@ -1,3 +1,7 @@
 const numbers = [5, 2, 7, 3, 9, 1, 4, 8];
 const sortedNumbers = numbers.sort();
-console.log(sortedNumbers);
+// console.log(sortedNumbers);
+
+const friends = ["badhon", "shanto", "nayan", "ullash", "uthso", "tusar"];
+const sortedFriends = friends.sort();
+console.log(sortedFriends);
