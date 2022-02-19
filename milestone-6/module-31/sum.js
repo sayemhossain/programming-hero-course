54d2988d25a5aec36cb3d88e6a6306400990a6a0
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-19 16:00:09 +0600

    new code added

--- milestone-6/module-31/sum.js
+++ milestone-6/module-31/sum.js
@@ -1,4 +1,8 @@
+// normal function
 function add(num1, num2) {
   return num1 + num2;
 }
-console.log(add(15, 17));
+// console.log(add(15, 17));
+// arrow function
+const add2 = (num1, num2) => num1 + num2;
+console.log(add2(10, 12));
