732ab97d142eb11bc420e791905ee0c404a07bca
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-20 00:36:10 +0600

    new code added

--- milestone-6/module-32/more-map.js
+++ milestone-6/module-32/more-map.js
@@ -1,3 +1,14 @@
 const friends = ["badhon", "nayan", "shanto", "utsho", "ullash"];
 const fLenghts = friends.map((friend) => friend.length);
-console.log(fLenghts);
+// console.log(fLenghts);
+const products = [
+  { name: "water bottle", price: 50, color: "tellow" },
+  { name: "mobile", price: 15000, color: "black" },
+  { name: "smart watch", price: 1300, color: "pink" },
+  { name: "sticky", price: 30, color: "white" },
+  { name: "water glass", price: 10, color: "blue" },
+];
+const productName = products.map((product) => product.name);
+const productPrices = products.map((product) => product.price);
+console.log(productName);
+console.log(productPrices);
