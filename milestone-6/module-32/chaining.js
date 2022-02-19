a7ac7dacdb452d0926fb52d4d0fa058cb98bd9ba
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-19 22:39:00 +0600

    new code added

---
+++ milestone-6/module-32/chaining.js
@@ -0,0 +1,8 @@
+// declare variable based on the name of object property
+const myObject = { x: 2, y: 50, z: 400 };
+const { x } = myObject;
+// console.log(x);
+
+// destructuring array
+const [p, q] = [23, 56, 76, 54];
+console.log(p, q);
---
+++ milestone-6/module-32/map.js
@@ -0,0 +1,7 @@
+const numbers = [4, 6, 8, 10];
+const output = [];
+for (const number of numbers) {
+  const result = number * 2;
+  output.push(result);
+}
+console.log(output);
