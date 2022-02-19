cb90110073df9fdad44385f5871439a1bee73e58
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-19 16:07:26 +0600

    new code added

--- milestone-6/module-31/arrowFunction.js
+++ milestone-6/module-31/arrowFunction.js
@@ -1,6 +1,10 @@
 const add = (num1, num2) => {
   return num1 + num2;
 };
-
 const sum = add(33, 65);
 console.log(sum);
+const multiply = (num1, num2, num3, num4) => {
+  return num1 * num2 * num3 * num4;
+};
+const result = multiply(3, 2, 4, 6);
+console.log(result);
