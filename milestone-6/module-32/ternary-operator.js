6414fa2f7608878f65ba251cb4d229ddc9366f45
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-20 15:20:49 +0600

    new code added

--- milestone-6/module-32/ternary-operator.js
+++ milestone-6/module-32/ternary-operator.js
@@ -1,3 +1,10 @@
-let age = 15;
-var type = age >= 18 ? "adult" : age < 10 ? "child" : "young";
+let age = 2;
+var type =
+  age >= 18
+    ? "adult"
+    : age < 10 && age > 2
+    ? "child"
+    : age <= 2
+    ? "baby"
+    : "young";
 console.log(type);
