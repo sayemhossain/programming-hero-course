7d0f6c9beeb384b4065cb23d9a28d2ba418a2ea2
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-22 01:23:06 +0600

    new code

--- milestone-6/module-33/post.js
+++ milestone-6/module-33/post.js
@@ -3,7 +3,19 @@ function loadPost() {
     .then((response) => response.json())
     .then((data) => displayPosts(data));
 }
-function displayPosts(data) {
-  console.log(data);
-}
 loadPost();
+
+function displayPosts(posts) {
+  //   console.log(data);
+  const postContainer = document.getElementById("posts");
+
+  for (const post of posts) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    `;
+    postContainer.appendChild(div);
+    console.log(post);
+  }
+}
