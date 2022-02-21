383f226652c154bea0ba315c292d59a596eb3289
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-21 23:49:28 +0600

    new code added

--- milestone-6/module-33/api.js
+++ milestone-6/module-33/api.js
@@ -1,1 +1,5 @@
-console.log("sayem");
+function loadData() {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
