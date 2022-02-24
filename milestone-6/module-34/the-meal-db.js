725fd85b1cc94fbf83633d4bb6c35b16bf318145
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-24 14:03:52 +0600

    new code added

--- milestone-6/module-34/the-meal-db.js
+++ milestone-6/module-34/the-meal-db.js
@@ -16,5 +16,17 @@ const displaySearchResult = (meals) => {
   const searchResult = document.getElementById("search-result");
   meals.forEach((meal) => {
     console.log(meal);
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+    <div class="card" style="height:40rem">
+            <img src="${meal.strMealThumb}" class="card-img-top " alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">${meal.strInstructions.slice(0, 250)}</p>
+            </div>
+    </div>
+    `;
+    searchResult.appendChild(div);
   });
 };
