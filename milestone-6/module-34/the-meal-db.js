84c90be465df59ad43234240fd4296ef0429d7cb
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-24 12:02:25 +0600

    new code added

--- milestone-6/module-34/the-meal-db.js
+++ milestone-6/module-34/the-meal-db.js
@@ -1,3 +1,13 @@
-fetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const searchFood = () => {
+  const searchFeild = document.getElementById("search-feild");
+  const searchText = searchFeild.value;
+  console.log(searchText);
+  searchFeild.value = "";
+
+  // fetching data from api
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+};
