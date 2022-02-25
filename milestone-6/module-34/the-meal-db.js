515df113731ba9708e949884d09835d1da16f857
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-25 20:10:43 +0600

    new code added

--- milestone-6/module-34/the-meal-db.js
+++ milestone-6/module-34/the-meal-db.js
@@ -53,8 +53,8 @@ const displayMealDetail = (meal) => {
   <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${meal.strMeal}</h5>
-    <p class="card-text">${meal.strInstructions.slice(0, 250)}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+    <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
   </div>
   `;
   mealDetails.appendChild(div);
