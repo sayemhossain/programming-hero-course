ccd1f9942f8fb183797f39b91652a3559cde44c1
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-25 19:45:41 +0600

    new code added

--- milestone-6/module-34/the-meal-db.js
+++ milestone-6/module-34/the-meal-db.js
@@ -34,4 +34,9 @@ const displaySearchResult = (meals) => {
 };
 const loadMealDetail = (mealId) => {
   console.log(mealId);
+  const url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}";
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 };
