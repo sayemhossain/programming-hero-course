dfd98462a3c92fb27c5df841e9f228dd70e6386e
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-26 04:50:11 +0600

    new code

--- milestone-6/module-36/key-value-entries.js
+++ milestone-6/module-36/key-value-entries.js
@@ -6,4 +6,12 @@ const bottle = {
 };
 // getting all keys
 const keys = Object.keys(bottle);
-console.log(keys);
+// console.log(keys);
+
+// getting all value
+const values = Object.values(bottle);
+// console.log(values);
+
+// getting keys and values at a time
+const pairs = Object.entries(bottle);
+console.log(pairs);
