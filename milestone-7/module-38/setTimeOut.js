c2d3ddd66784bd8aa250f321df6ccd7174b3fb3d
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-04 02:25:31 +0600

    new code added

---
+++ milestone-7/module-38/setTimeOut.js
@@ -0,0 +1,8 @@
+function doSomething() {
+  console.log("I am coding JavaScript");
+}
+console.log("First: I am first");
+console.log("2nd: I am 2nd");
+setTimeout(doSomething);
+console.log("3rd: I am 3rd");
+console.log("4th: I am 4thd");
