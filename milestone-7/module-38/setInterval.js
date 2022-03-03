fe9e1538631061823755ceeda1a75bf3976f31ca
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-04 02:46:45 +0600

    new code

--- milestone-7/module-38/setInterval.js
+++ milestone-7/module-38/setInterval.js
@@ -5,9 +5,9 @@ console.log("first");
 
 let seconds = 0;
 const timeId = setInterval(() => {
-  seconds++;
-  console.log(seconds);
-  if (seconds > 15) {
+  //   seconds++;
+  console.log(++seconds);
+  if (seconds > 10) {
     clearInterval(timeId);
   }
 }, 1000);
