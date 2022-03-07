c53d82aacdaddc9758147d82b6d848982717170b
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-07 21:23:23 +0600

    new code

--- milestone-7/module-39/explore.js
+++ milestone-7/module-39/explore.js
@@ -1,7 +1,23 @@
-// alert("Hi this is me sayem");
+// alert
 const alertTest = () => {
   alert("Hi this is me Sayem");
 };
+
+// confirm
 const askPicnic = () => {
-  confirm("Are you going to picnic?");
+  const response = confirm("Are you going to picnic?");
+  console.log(response);
+  if (response == true) {
+    alert("Taka bKash kore deu..");
+  } else {
+    alert("DMG! dure giya mor ");
+  }
+};
+
+// prompt
+const askName = () => {
+  const name = prompt("what's your name");
+  if (name) {
+    console.log(name);
+  }
 };
