eae61dbd2aac146e4e255bc3d2d2209f707e5643
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-08 15:50:46 +0600

    new code

--- milestone-7/module-39/shopping.js
+++ milestone-7/module-39/shopping.js
@@ -1,5 +1,16 @@
 const addItem = () => {
   const inputField = document.getElementById("product-name");
-  const inputValue = inputField.value;
-  console.log(inputValue);
+  const name = inputField.value;
+  console.log(name);
+  //display in the ui
+  displayProduct(name);
+  //add to local storage
+
+  inputField.value = "";
+};
+const displayProduct = (name) => {
+  const ul = document.getElementById("products");
+  const li = document.createElement("li");
+  li.innerText = name;
+  ul.appendChild(li);
 };
