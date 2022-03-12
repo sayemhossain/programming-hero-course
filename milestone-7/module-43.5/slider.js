b0a2b0723518fbb53aab57eadb29a580dfe1a6d8
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-12 19:45:28 +0600

    new code added

--- milestone-7/module-43.5/slider.js
+++ milestone-7/module-43.5/slider.js
@@ -7,9 +7,13 @@ let photos = [
   "img/setup.png",
   "img/tripod.png",
 ];
+const imgId = document.getElementById("photo-slider");
 let photosIndex = 0;
 setInterval(() => {
+  if (photosIndex > photos.length - 1) {
+    photosIndex = 0;
+  }
   const photosUrl = photos[photosIndex];
-  console.log(photosUrl);
+  imgId.setAttribute("src", photosUrl);
   photosIndex++;
 }, 1000);
