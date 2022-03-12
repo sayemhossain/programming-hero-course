e31f7f87812f046aedeaf389b015d027b1556e70
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-12 19:39:46 +0600

    new code added

--- milestone-7/module-43.5/slider.js
+++ milestone-7/module-43.5/slider.js
@@ -1,1 +1,15 @@
-console.log("Hi this is me Sayem");
+let photos = [
+  "img/action.png",
+  "img/airpod.png",
+  "img/camera.png",
+  "img/drone.png",
+  "img/mic.png",
+  "img/setup.png",
+  "img/tripod.png",
+];
+let photosIndex = 0;
+setInterval(() => {
+  const photosUrl = photos[photosIndex];
+  console.log(photosUrl);
+  photosIndex++;
+}, 1000);
